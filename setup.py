a64cdb171d012c55d7c78159a906f995ffa241d6
Meir Elbaz <meire@mobileye.com>
2022-02-08 20:51:55 +0200

    New setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+from properties import __version__
+
+setup(
+    name="properties",
+    version=__version__,
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": ["properties = properties.__main__:main"]
+    },
+)
