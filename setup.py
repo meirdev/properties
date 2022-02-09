003c21c6733b69ebbff938b79edf7b9bc6bd29f8
Meir Elbaz <meire@mobileye.com>
2022-02-09 09:07:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@ from setuptools import setup, find_packages
 
 from properties import __version__
 
+URL = "https://github.com/meirdev/properties"
+PYTHON_REQUIRES = ">=3.7"
+
 setup(
     name="properties",
+    url=URL,
+    python_requires=PYTHON_REQUIRES,
     version=__version__,
     packages=find_packages(),
     entry_points={"console_scripts": ["properties = properties.__main__:main"]},
